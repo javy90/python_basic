1e0d25ce7b882a93df174d16be4f02cd614f47d8
javy90 <100823936+javy90@users.noreply.github.com>
2022-03-20 20:31:38 +0900

    Add files via upload

---
+++ hangul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+
+from matplotlib import rc
+rc('font', family = 'Malgun Gothic')
+plt.rcParams['axes.unicode_minus'] = False
+
